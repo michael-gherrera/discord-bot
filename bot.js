7eec660ad2560dbcbeeb5e2f280b930e8ecc7ccf
Bryan Lam <blam@adobe.com>
2018-02-04 19:53:21 -0800

    format stock ticker info

---
+++ bot.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+const utils = require('./utils.json');
+var request = require('request');
+ 
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+ 
+client.on('message', message => {
+  if (message.content.indexOf('!') === 0 && message.content.split(' ').length === 2) {
+    var contents = message.content.split(' ');
+    if (contents[0] == "!stock") {
+      var tickerUrl = utils.prices_url + "function=TIME_SERIES_DAILY&symbol=" + contents[1] + "&apikey=" + auth.api_key;
+      request.get(tickerUrl, function (err, res, body) {
+        var jsonBody = JSON.parse(body);
+        var currentDate = jsonBody["Meta Data"]["3. Last Refreshed"];
+        message.channel.send(formatOutput(JSON.stringify(jsonBody["Time Series (Daily)"][currentDate])));
+      });
+    } else if (contents[0] == "!coin") {
+      
+    }
+  }
+});
+
+var formatOutput = function(json) {
+  var output = json.split(',').join('\n');
+  output = output.substring(1, output.length-1);
+  return ("```" + output  + "```");
+}
+
+client.login(auth.bot_token);
