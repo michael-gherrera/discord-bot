4dc57f343a5abc322205c026496f1bd4f156f9c1
Bryan Lam <blam@adobe.com>
2018-02-04 23:59:31 -0800

    bot v1

--- bot.js
+++ bot.js
@@ -3,11 +3,11 @@ const client = new Discord.Client();
 const auth = require('./auth.json');
 const utils = require('./utils.json');
 var request = require('request');
- 
+
 client.on('ready', () => {
   console.log('I am ready!');
 });
- 
+
 client.on('message', message => {
   if (message.content.indexOf('!') === 0 && message.content.split(' ').length === 2) {
     var contents = message.content.split(' ');
@@ -19,15 +19,36 @@ client.on('message', message => {
         message.channel.send(formatOutput(JSON.stringify(jsonBody["Time Series (Daily)"][currentDate])));
       });
     } else if (contents[0] == "!coin") {
-      
+      var coinUrl = utils.prices_url + "function=DIGITAL_CURRENCY_DAILY&symbol=" + contents[1] + "&market=USD&apikey=" + auth.api_key;
+      request.get(coinUrl, function (err, res, body) {
+        var jsonBody = JSON.parse(body);
+        var currentDate = (jsonBody["Meta Data"]["6. Last Refreshed"].split(' '))[0];
+        var output = formatOutput(JSON.stringify(jsonBody["Time Series (Digital Currency Daily)"][currentDate]));
+        
+        message.channel.send(output)
+      });
     }
   }
 });
 
-var formatOutput = function(json) {
+var formatOutput = function (json) {
   var output = json.split(',').join('\n');
-  output = output.substring(1, output.length-1);
-  return ("```" + output  + "```");
+  output = output.substring(1, output.length - 1);
+  return ("```" + output + "```");
+}
+
+/*
+ * Current date in YYYY-MM-DD, if it's saturday or sunday, we'll need the current date to change to friday?
+ */
+var getCurrentDate = function () {
+  var date = new Date();
+//  if (type === "stock") {
+//    //Sunday
+//    if (date.getDay() === 0) {
+//      
+//    }
+//  }
+  return (date.toISOString().substring(0, 10));
 }
 
 client.login(auth.bot_token);
