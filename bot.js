3f32fc2f0296e036d0602a3b988f1c76c790c32d
BryanSLam <bryan.s.lam@gmail.com>
2018-04-17 00:01:15 -0700

    change API to use iex trading and increased error resiliency

--- bot.js
+++ bot.js
@@ -1,54 +1,52 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
-const utils = require('./utils.json');
+const config = require('./config.json');
 var request = require('request');
+
+var commandRegex = /![a-zA-Z]+ [a-zA-Z]+/g
+var stockRegex = /[a-zA-Z]{1,4}/g
 
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
 client.on('message', message => {
-  if (message.content.indexOf('!') === 0 && message.content.split(' ').length === 2) {
+  if (message.content.match(commandRegex)) {
     var contents = message.content.split(' ');
-    if (contents[0] == "!stock") {
-      var tickerUrl = utils.prices_url + "function=TIME_SERIES_DAILY&symbol=" + contents[1] + "&apikey=" + auth.api_key;
-      request.get(tickerUrl, function (err, res, body) {
-        var jsonBody = JSON.parse(body);
-        var currentDate = jsonBody["Meta Data"]["3. Last Refreshed"];
-        message.channel.send(formatOutput(JSON.stringify(jsonBody["Time Series (Daily)"][currentDate])));
-      });
-    } else if (contents[0] == "!coin") {
-      var coinUrl = utils.prices_url + "function=DIGITAL_CURRENCY_DAILY&symbol=" + contents[1] + "&market=USD&apikey=" + auth.api_key;
-      request.get(coinUrl, function (err, res, body) {
-        var jsonBody = JSON.parse(body);
-        var currentDate = (jsonBody["Meta Data"]["6. Last Refreshed"].split(' '))[0];
-        var output = formatOutput(JSON.stringify(jsonBody["Time Series (Digital Currency Daily)"][currentDate]));
-        
-        message.channel.send(output)
-      });
+    if (contents[0].toLowerCase() === "!stock") {
+      if (contents[1].match(stockRegex)) {
+        var tickerUrl = config.api_url + contents[1] + "/batch?types=quote"
+        request.get(tickerUrl, function (err, res, body) {
+          try {
+            var info = JSON.parse(body).quote;
+          } catch (e) {
+            message.channel.send(body);
+            return
+          }
+          var output = {
+            "symbol": info.symbol,
+            "current": info.latestPrice,
+            "high": info.high,
+            "low": info.low,
+            "percentChange": info.changePercent * 100 + "%",
+            "volume": info.latestVolume
+          }
+          message.channel.send(formatOutput(JSON.stringify(output)));
+        });
+      } else {
+        message.channel.send('Invalid Symbol Fuck You');
+      }
+    } else {
+      message.channel.send('Invalid Command Fuck You');
     }
   }
 });
 
-var formatOutput = function (json) {
-  var output = json.split(',').join('\n');
+var formatOutput = function (message) {
+  var output = message.split(',').join('\n');
   output = output.substring(1, output.length - 1);
   return ("```" + output + "```");
-}
-
-/*
- * Current date in YYYY-MM-DD, if it's saturday or sunday, we'll need the current date to change to friday?
- */
-var getCurrentDate = function () {
-  var date = new Date();
-//  if (type === "stock") {
-//    //Sunday
-//    if (date.getDay() === 0) {
-//      
-//    }
-//  }
-  return (date.toISOString().substring(0, 10));
 }
 
 client.login(auth.bot_token);
