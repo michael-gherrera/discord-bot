010757bb67f442345a6c396815568e31b058ae54
Bryan Lam <blam@adobe.com>
2018-04-25 21:57:02 -0700

    add coin support

--- bot.js
+++ bot.js
@@ -5,7 +5,8 @@ const config = require('./config.json');
 var request = require('request');
 
 var commandRegex = /![a-zA-Z]+ [a-zA-Z]+/g
-var stockRegex = /[a-zA-Z]{1,4}/g
+var stockRegex = /^[a-zA-Z]{1,4}$/g
+var coinRegex = /^[a-zA-Z]{1,4}$/g
 
 client.on('ready', () => {
   console.log('I am ready!');
@@ -14,9 +15,10 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.content.match(commandRegex)) {
     var contents = message.content.split(' ');
+    var output;
     if (contents[0].toLowerCase() === "!stock") {
       if (contents[1].match(stockRegex)) {
-        var tickerUrl = config.api_url + contents[1] + "/batch?types=quote"
+        var tickerUrl = config.stock_api_url + contents[1] + "/batch?types=quote";
         request.get(tickerUrl, function (err, res, body) {
           try {
             var info = JSON.parse(body).quote;
@@ -24,7 +26,7 @@ client.on('message', message => {
             message.channel.send(body);
             return
           }
-          var output = {
+          output = {
             "Symbol": info.symbol,
             "Company Name": info.companyName,
             "Current": info.latestPrice,
@@ -39,6 +41,24 @@ client.on('message', message => {
         });
       } else {
         message.channel.send('Invalid Symbol Fuck You');
+      }
+    } else if(contents[0].toLowerCase() === "!coin") {
+      if(contents[1].match(coinRegex)) {
+        var coinUrl = config.coin_api_url + contents[1].toUpperCase() + "&tsyms=USD";
+        request.get(coinUrl, function (err, res, body) {
+          var info = JSON.parse(body);
+          if (info.Message != undefined) {
+            message.channel.send(info.Message);
+          } else {
+            output = {
+              "Symbol": contents[1].toUpperCase(),
+              "Price (USD)": info.USD
+            }
+            message.channel.send(formatOutput(JSON.stringify(output)));
+          }
+        });
+      } else {
+        message.channel.send('Invalid Coin Symbol Buckfoy')
       }
     } else {
       message.channel.send('Invalid Command Fuck You');
