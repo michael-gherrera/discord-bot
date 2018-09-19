29cde351cb163e59c5cdc61407641e39a6fd1557
BryanSLam <bryan.s.lam@gmail.com>
2018-09-18 23:27:03 -0700

    woops re-add hotfix:

--- commands/commander.go
+++ commands/commander.go
@@ -9,37 +9,39 @@ import (
 
 func Commander() func(s *dg.Session, m *dg.MessageCreate) {
 	return func(s *dg.Session, m *dg.MessageCreate) {
-		// Ignore all messages created by the bot itself
-		// This isn't required in this specific example but it's a good practice.
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
+		if match, _ := regexp.MatchString("![a-zA-Z]+[ a-zA-Z\"]*[ 0-9/]*", m.Content); match {
+			// Ignore all messages created by the bot itself
+			// This isn't required in this specific example but it's a good practice.
+			if m.Author.ID == s.State.User.ID {
+				return
+			}
 
-		if match, _ := regexp.MatchString("!ping", m.Content); match {
-			Ping(s, m)
-			return
-		}
+			if match, _ := regexp.MatchString("!ping", m.Content); match {
+				Ping(s, m)
+				return
+			}
 
-		if match, _ := regexp.MatchString("(?i)^!stock [a-zA-Z]+$", m.Content); match {
-			Stock(s, m)
-			return
-		}
+			if match, _ := regexp.MatchString("(?i)^!stock [a-zA-Z]+$", m.Content); match {
+				Stock(s, m)
+				return
+			}
 
-		if match, _ := regexp.MatchString("(?i)^!er [a-zA-Z]+$", m.Content); match {
-			Er(s, m)
-			return
-		}
+			if match, _ := regexp.MatchString("(?i)^!er [a-zA-Z]+$", m.Content); match {
+				Er(s, m)
+				return
+			}
 
-		if match, _ := regexp.MatchString("(?i)^!wizdaddy$", m.Content); match {
-			Wizdaddy(s, m)
-			return
-		}
+			if match, _ := regexp.MatchString("(?i)^!wizdaddy$", m.Content); match {
+				Wizdaddy(s, m)
+				return
+			}
 
-		if match, _ := regexp.MatchString("(?i)^!coin [a-zA-Z]+$", m.Content); match {
-			Coin(s, m)
-			return
-		}
+			if match, _ := regexp.MatchString("(?i)^!coin [a-zA-Z]+$", m.Content); match {
+				Coin(s, m)
+				return
+			}
 
-		s.ChannelMessageSend(m.ChannelID, config.GetConfig().InvalidCommandMessage)
+			s.ChannelMessageSend(m.ChannelID, config.GetConfig().InvalidCommandMessage)
+		}
 	}
 }
